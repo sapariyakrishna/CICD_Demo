51f31abc91c1d2d8970ab83d7402027ab6fb4972
Tagline <krishna.tagline@gmail.com>
2022-12-21 09:54:42 +0530

    - set up of CI/CD

---
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+cd ios
+SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
+PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
