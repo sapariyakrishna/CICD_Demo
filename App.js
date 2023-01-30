f375d46a7f179bf476ab88e68b52eae4b4111dcb
Tagline <krishna.tagline@gmail.com>
2023-01-30 15:40:52 +0530

    ------

--- App.js
+++ App.js
@@ -185,7 +185,6 @@ const App = () => {
         style={{height: 50, width: 100, backgroundColor: 'blue'}}
         onPress={() => {}}>
         <Text>Display notification</Text>
-        <Text>DemoApp</Text>
       </TouchableOpacity>
       {/* <MapView
         coordinate={position}
